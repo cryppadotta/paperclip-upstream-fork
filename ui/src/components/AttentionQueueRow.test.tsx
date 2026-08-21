paperclipai/paperclip

fix(ui): refine decision card interactions
Co-Authored-By: Paperclip <noreply@paperclip.ing>

ui/src/components/AttentionQueueRow.test.tsx
@@ -183,7 +183,7 @@ describe("AttentionQueueRow", () => {
     expect(trigger).toBeTruthy();
   });
 
-  it("toggles expand when the collapsed header of an inline row is clicked", () => {
+  it("toggles expand when the decision body or empty card space is clicked", () => {
     const onToggleExpand = vi.fn();
     render(
       <AttentionQueueRow
@@ -194,13 +194,17 @@ describe("AttentionQueueRow", () => {
         onDismiss={noop}
       />,
     );
-    const header = container?.querySelector('[role="button"][aria-expanded]');
-    expect(header).toBeTruthy();
-    expect(header?.getAttribute("aria-expanded")).toBe("false");
+    const body = container?.querySelector("[data-attention-card-body]");
+    const detail = Array.from(container?.querySelectorAll("p") ?? []).find((element) =>
+      element.textContent?.includes("Approval is pending"),
+    );
+    expect(body).toBeTruthy();
+    expect(detail).toBeTruthy();
     act(() => {
-      header?.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+      detail?.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+      body?.dispatchEvent(new MouseEvent("click", { bubbles: true }));
     });
-    expect(onToggleExpand).toHaveBeenCalledTimes(1);
+    expect(onToggleExpand).toHaveBeenCalledTimes(2);
   });
 
   it("exposes the visible expand chevron as an accessible button", () => {
@@ -222,19 +226,41 @@ describe("AttentionQueueRow", () => {
     expect(onToggleExpand).toHaveBeenCalledWith(expect.objectContaining({ id: "a1" }));
   });
 
-  it("does not navigate on title click — the title is plain text, not a link", () => {
+  it("opens the title, issue identifier, and timestamp in a new tab without toggling", () => {
+    const onToggleExpand = vi.fn();
     render(
       <AttentionQueueRow
-        item={buildItem()}
+        item={buildItem({
+          relatedIssue: {
+            kind: "issue",
+            id: "issue-1",
+            companyId: "c1",
+            title: "Implement linked decision",
+            identifier: "PAP-42",
+            status: "in_progress",
+            href: "/PAP/issues/PAP-42",
+            metadata: {},
+          },
+        })}
         companyId="c1"
         expanded={false}
-        onToggleExpand={noop}
+        onToggleExpand={onToggleExpand}
         onDismiss={noop}
       />,
     );
-    const links = Array.from(container?.querySelectorAll("a") ?? []);
-    // No anchor should carry the subject title (only the identifier link, absent here).
-    expect(links.some((a) => a.textContent?.includes("Hire agent: Research Analyst"))).toBe(false);
+    const links = Array.from(container?.querySelectorAll('a[href="/PAP/issues/PAP-42"]') ?? []);
+    expect(links.map((link) => link.textContent?.trim())).toEqual([
+      "PAP-42",
+      expect.stringMatching(/ago$/),
+      "Hire agent: Research Analyst",
+    ]);
+    for (const link of links) {
+      expect(link.getAttribute("target")).toBe("_blank");
+      expect(link.getAttribute("rel")).toBe("noopener noreferrer");
+      link.addEventListener("click", (event) => event.preventDefault());
+      act(() => link.dispatchEvent(new MouseEvent("click", { bubbles: true, cancelable: true })));
+    }
+    expect(onToggleExpand).not.toHaveBeenCalled();
   });
 
   it("renders project identity once without a filter button", () => {
@@ -309,9 +335,11 @@ describe("AttentionQueueRow", () => {
       />,
     );
 
-    const header = container?.querySelector('[role="button"][aria-expanded]');
-    expect(header?.textContent).not.toContain("Approve");
-    expect(header?.textContent).not.toContain("Reject");
+    const title = Array.from(container?.querySelectorAll("span") ?? []).find((element) =>
+      element.textContent === "Hire agent: Research Analyst",
+    );
+    expect(title?.parentElement?.textContent).not.toContain("Approve");
+    expect(title?.parentElement?.textContent).not.toContain("Reject");
 
     const decisionActions = container?.querySelector('[aria-label="Decision actions"]');
     expect(decisionActions?.textContent).toContain("Approve");

cryppadotta/paperclip-upstream-fork

ui/src/components/AttentionQueueRow.test.tsx
@@ -311,7 +311,7 @@ describe("AttentionQueueRow", () => {
     expect(trigger).toBeTruthy();
   });
 
-  it("toggles expand when the collapsed header of an inline row is clicked", () => {
+  it("toggles expand when the decision body or empty summary space is clicked", () => {
     const onToggleExpand = vi.fn();
     render(
       <AttentionQueueRow
@@ -322,13 +322,17 @@ describe("AttentionQueueRow", () => {
         onDismiss={noop}
       />,
     );
-    const header = container?.querySelector('[role="button"][aria-expanded]');
-    expect(header).toBeTruthy();
-    expect(header?.getAttribute("aria-expanded")).toBe("false");
+    const body = container?.querySelector("[data-attention-card-body]");
+    const detail = Array.from(container?.querySelectorAll("p") ?? []).find((element) =>
+      element.textContent?.includes("Approval is pending"),
+    );
+    expect(body).toBeTruthy();
+    expect(detail).toBeTruthy();
     act(() => {
-      header?.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+      detail?.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+      body?.dispatchEvent(new MouseEvent("click", { bubbles: true }));
     });
-    expect(onToggleExpand).toHaveBeenCalledTimes(1);
+    expect(onToggleExpand).toHaveBeenCalledTimes(2);
   });
 
   it("exposes the visible expand chevron as an accessible button", () => {
@@ -350,19 +354,41 @@ describe("AttentionQueueRow", () => {
     expect(onToggleExpand).toHaveBeenCalledWith(expect.objectContaining({ id: "a1" }));
   });
 
-  it("does not navigate on title click — the title is plain text, not a link", () => {
+  it("opens the title, task key, and timestamp in a new tab without toggling", () => {
+    const onToggleExpand = vi.fn();
     render(
       <AttentionQueueRow
-        item={buildItem()}
+        item={buildItem({
+          relatedIssue: {
+            kind: "issue",
+            id: "issue-1",
+            companyId: "c1",
+            title: "Implement linked decision",
+            identifier: "PAP-42",
+            status: "in_progress",
+            href: "/PAP/issues/PAP-42",
+            metadata: {},
+          },
+        })}
         companyId="c1"
         expanded={false}
-        onToggleExpand={noop}
+        onToggleExpand={onToggleExpand}
         onDismiss={noop}
       />,
     );
-    const links = Array.from(container?.querySelectorAll("a") ?? []);
-    // No anchor should carry the subject title (only the identifier link, absent here).
-    expect(links.some((a) => a.textContent?.includes("Hire agent: Research Analyst"))).toBe(false);
+    const links = Array.from(container?.querySelectorAll('a[href="/PAP/issues/PAP-42"]') ?? []);
+    expect(links.map((link) => link.textContent?.trim())).toEqual([
+      "PAP-42",
+      expect.any(String),
+      "Hire agent: Research Analyst",
+    ]);
+    for (const link of links) {
+      expect(link.getAttribute("target")).toBe("_blank");
+      expect(link.getAttribute("rel")).toBe("noopener noreferrer");
+      link.addEventListener("click", (event) => event.preventDefault());
+      act(() => link.dispatchEvent(new MouseEvent("click", { bubbles: true, cancelable: true })));
+    }
+    expect(onToggleExpand).not.toHaveBeenCalled();
   });
 
   // The eyebrow carries the decision kind and the task key only. Project
@@ -511,9 +537,9 @@ describe("AttentionQueueRow", () => {
       />,
     );
 
-    const header = container?.querySelector('[role="button"][aria-expanded]');
-    expect(header?.textContent).not.toContain("Approve");
-    expect(header?.textContent).not.toContain("Reject");
+    const body = container?.querySelector("[data-attention-card-body]");
+    expect(body?.textContent).not.toContain("Approve");
+    expect(body?.textContent).not.toContain("Reject");
 
     const decisionActions = container?.querySelector('[aria-label="Decision actions"]');
     expect(decisionActions?.textContent).toContain("Approve");
@@ -711,7 +737,7 @@ describe("AttentionQueueRow", () => {
         onDismiss={noop}
       />,
     );
-    expect(container?.querySelector('[role="button"][aria-expanded]')).toBeNull();
+    expect(container?.querySelector('button[aria-label="Expand decision"]')).toBeNull();
   });
 
   it("makes a non-inline row with images expandable", () => {
@@ -736,9 +762,9 @@ describe("AttentionQueueRow", () => {
         onDismiss={noop}
       />,
     );
-    const header = container?.querySelector('[role="button"][aria-expanded]');
-    expect(header).not.toBeNull();
-    act(() => (header as HTMLElement).click());
+    const body = container?.querySelector("[data-attention-card-body]");
+    expect(body).not.toBeNull();
+    act(() => (body as HTMLElement).click());
     expect(onToggleExpand).toHaveBeenCalledTimes(1);
   });
 